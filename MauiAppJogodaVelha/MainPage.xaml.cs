d266a388fe069787142a9c620301803842226ed6
IzaiasCF <izaiascorreafilho@gmail.com>
2025-09-24 10:53:40 -0300

    Add: X + O

--- MauiAppJogodaVelha/MainPage.xaml.cs
+++ MauiAppJogodaVelha/MainPage.xaml.cs
@@ -2,6 +2,8 @@
 {
     public partial class MainPage : ContentPage
     {
+        string vez = "X";
+
         public MainPage()
         {
             InitializeComponent();
@@ -9,7 +11,18 @@
 
         private void Button_Clicked(object sender, EventArgs e)
         {
+            Button btn = (Button)sender;
 
+            if (vez == "X")
+            {
+                btn.Text = "X";
+                vez = "O";
+            }
+            else 
+            {
+                btn.Text = "O";
+                vez = "X";
+            }
         }
     }
 }
