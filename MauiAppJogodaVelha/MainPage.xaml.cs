1899bc15fd97d2415bace58de36de0da29a3bdc7
IzaiasCF <izaiascorreafilho@gmail.com>
2025-09-24 09:41:36 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppJogodaVelha/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiAppJogodaVelha
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
